864ef5e531bb6e08449cd044ab691f31669c1488
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2019-10-12 16:34:01 -0400

    updated javascript with src and button inputs. still attempting to connect counter

--- crystalgame.js
+++ crystalgame.js
@@ -1,28 +1,85 @@
 $(document).ready(function() {
-    let Random = Math.floor(Math.random() * 120 + 19)
+    let Random = Math.floor(Math.random() * 100 + 1)
 
-    $("#targetScore").text(Random);
+    $("#targetNumber").text(Random);
 
     let crystal1 = Math.floor(Math.random() * 12 + 1)
     let crystal2 = Math.floor(Math.random() * 12 + 1)
     let crystal3 = Math.floor(Math.random() * 12 + 1)
     let crystal4 = Math.floor(Math.random() * 12 + 1)
+
     console.log("Pink: " + crystal1, "Blue: " + crystal2, "Purple: " + crystal3, "Green: " + crystal4);
 
     let playerScore = 0;
     let wins = 0;
     let losses = 0;
 
+    // wins and losses counter //
     function winner() { 
         wins++;
-        alert(Winner!); 
+        alert("Winner!"); 
         reset();
     }
 
     function loser() {
         losses++;
-        alert(Loser);
+        alert("Loser");
         reset();
     }
+
+    // click commands for each gem //
+
+    $("#pink").on("click", function() {
+        playerScore = playerScore + crystal1;
+        $("totalScore").text(playerScore); 
     
-}
+        if (playerScore == Random) {
+            alert("You Win!")
+        }
+
+        else if (playerScore > Random) { 
+            alert("Nope, you lose!");
+        }
+    })
+
+    $(".blue").on("click", function() {
+        playerScore = playerScore + crystal2;
+        $("totalScore").text(playerScore); 
+    
+        if (playerScore == Random) {
+            alert("You Win!")
+        }
+
+        else if (playerScore > Random) { 
+            alert("Nope, you lose!");
+        }
+    })
+    
+    $(".purple").on("click", function() {
+        playerScore = playerScore + crystal3;
+        $("totalScore").text(playerScore); 
+    
+        if (playerScore == Random) {
+            alert("You Win!")
+        }
+
+        else if (playerScore > Random) { 
+            alert("Nope, you lose!");
+        }
+    })
+
+    $(".green").on("click", function() {
+        playerScore = playerScore + crystal4;
+        $("totalScore").text(playerScore); 
+    
+        if (playerScore == Random) {
+            alert("You Win!")
+        }
+
+        else if (playerScore > Random) { 
+            alert("Nope, you lose!");
+        }
+    })
+
+
+});
